1e326e8ba9773ffecdb747f7b6290179bc83daa0
Atisha <atishasinha10@gmail.com>
2026-02-16 12:32:31 +0530

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const transactionRoutes = require("./routes/transactionsRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // Routes
@@ -20,6 +22,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
