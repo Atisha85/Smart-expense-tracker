a44f2b95da35cbcab5aa88559ed5b6c376b5a9fc
Atisha <atishasinha10@gmail.com>
2026-03-01 13:50:29 +0530

    fix backend dependencies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -21,8 +22,18 @@ app.use("/api/transactions", transactionRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+// Serve frontend
+app.use(express.static(
+  path.join(__dirname, "../smart-expense-tracker/build")
+));
 
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../smart-expense-tracker/build/index.html")
+  );
+});
 const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
