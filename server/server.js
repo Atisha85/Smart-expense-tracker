ee4b24e13f1701ca41bf119db67fa2b516a9f993
Atisha <atishasinha10@gmail.com>
2026-03-02 00:06:40 +0530

    fix server wildcard

--- server/server.js
+++ server/server.js
@@ -1,39 +1,50 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const path = require("path");
+
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
-const transactionRoutes = require("./routes/transactionsRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors());
 app.use(express.json());
 
-// Routes
+/* API routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-// MongoDB connect
-mongoose.connect(process.env.MONGO_URI)
+/* MongoDB */
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-// Serve frontend
-app.use(express.static(
-  path.join(__dirname, "../smart-expense-tracker/build")
-));
+  .catch((err) => console.log(err));
+
+/* ---------- FRONTEND BUILD ---------- */
+
+app.use(
+  express.static(
+    path.join(__dirname, "../smart-expense-tracker/build")
+  )
+);
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../smart-expense-tracker/build","index.html")
+    path.join(
+      __dirname,
+      "../smart-expense-tracker/build",
+      "index.html"
+    )
   );
 });
-const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
+/* ---------- PORT ---------- */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log("Server running on port", PORT)
+);
