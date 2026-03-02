9acae0305fe75d2db7130a7527d18ad8aa3a5da0
Atisha <atishasinha10@gmail.com>
2026-03-02 16:17:18 +0530

    fix frontend serve

--- server/server.js
+++ server/server.js
@@ -13,17 +13,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* API routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionsRoutes);
 
+
 /* MongoDB */
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-/* ---------- FRONTEND BUILD ---------- */
+
+/* ✅ FRONTEND BUILD PATH */
 
 const buildPath = path.join(
   __dirname,
@@ -38,10 +40,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
-/* ---------- PORT ---------- */
+
+/* PORT */
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log("Server running on port", PORT)
+  console.log("Server running on", PORT)
 );
