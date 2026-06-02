490d5659a294bebe18cace6ba5a20b2bfc288df9
Atisha <atishasinha10@gmail.com>
2026-06-02 10:39:13 +0530

    fix build and api urls

--- smart-expense-tracker/src/context/ExpenseContext.js
+++ smart-expense-tracker/src/context/ExpenseContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
 
 export const ExpenseContext = createContext();
 
